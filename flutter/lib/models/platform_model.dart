fd98c766de88f89f36f7b699d402c95947ec6c67
August_rush <864011713@qq.com>
2024-04-09 15:20:31 +0800

    feat: a

--- flutter/lib/models/platform_model.dart
+++ flutter/lib/models/platform_model.dart
@@ -1,7 +1,16 @@
+/*
+ * @Author: August_rush
+ * @Date: 2024-03-04 14:16:44
+ * @LastEditors: August_rush 864011713@qq.com
+ * @LastEditTime: 2024-04-09 15:20:24
+ * @FilePath: \rustdesk-r\flutter\lib\models\platform_model.dart
+ * @Description: 
+ * 
+ */
 import 'package:flutter_hbb/generated_bridge.dart';
 import 'native_model.dart' if (dart.library.html) 'web_model.dart';
 
 final platformFFI = PlatformFFI.instance;
 final localeName = PlatformFFI.localeName;
 
-RustdeskImpl get bind => platformFFI.ffiBind;
+YIKEImpl get bind => platformFFI.ffiBind;
